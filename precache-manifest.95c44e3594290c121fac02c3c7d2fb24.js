a0aa0376197fe035db1c5117b4bc565474403a12
ashishdevden <ashishdevden@gmail.com>
2020-10-19 15:49:32 +0530

    Updates

---
+++ precache-manifest.95c44e3594290c121fac02c3c7d2fb24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79df2e9fa5b5cd04a98137d552c7e8c4",
+    "url": "/Devden-Solutions/ThothTechHandLandmarkBuilds/index.html"
+  },
+  {
+    "revision": "d0a60190bd3e2db01842",
+    "url": "/Devden-Solutions/ThothTechHandLandmarkBuilds/static/css/main.9b1ab70d.chunk.css"
+  },
+  {
+    "revision": "527727e97893e4b7f1d6",
+    "url": "/Devden-Solutions/ThothTechHandLandmarkBuilds/static/js/2.b09b0eaf.chunk.js"
+  },
+  {
+    "revision": "93ce851949c81c033c28f6758a3c33f3",
+    "url": "/Devden-Solutions/ThothTechHandLandmarkBuilds/static/js/2.b09b0eaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0a60190bd3e2db01842",
+    "url": "/Devden-Solutions/ThothTechHandLandmarkBuilds/static/js/main.98864148.chunk.js"
+  },
+  {
+    "revision": "afd79ccf3859b8ee37ae",
+    "url": "/Devden-Solutions/ThothTechHandLandmarkBuilds/static/js/runtime-main.818ebe2b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/LastTest/precache-manifest.e4d1c6ff61eac6d6d2007fb21355fe46.js"
+  "/Devden-Solutions/ThothTechHandLandmarkBuilds/precache-manifest.95c44e3594290c121fac02c3c7d2fb24.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/LastTest/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Devden-Solutions/ThothTechHandLandmarkBuilds/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
